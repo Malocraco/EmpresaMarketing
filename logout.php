f2328884abcee7aea6eb157d4644e4fbd26d04af
Malocraco <juanpabloramirezlozano77@gmail.com>
2025-05-15 23:22:16 -0500

    actualización del logout

--- logout.php
+++ logout.php
@@ -1,17 +1,6 @@
 <?php
 session_start();
-
-// Destruir todas las variables de sesión
-$_SESSION = [];
-session_unset();
 session_destroy();
-
-// Evitar uso del botón atrás
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Pragma: no-cache");
-
-// Redirigir al login
-header("Location: login.php");
+header("Location: index.php");
 exit();
-
 ?>
