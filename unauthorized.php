4cd3abd59ea3316f2ffaa560d2d448abf51ea804
Malocraco <juanpabloramirezlozano77@gmail.com>
2025-05-28 20:52:27 -0500

    Subiendo nuevo archivo

---
+++ unauthorized.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Acceso no autorizado</title>
+</head>
+<body>
+    <h2>No tienes permisos para acceder a esta sección.</h2>
+    <a href="index.php">Volver al inicio</a>
+</body>
+</html>
