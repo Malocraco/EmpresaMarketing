77910ed095c978648bc32a6dfb9bb76f849461fb
Malocraco <juanpabloramirezlozano77@gmail.com>
2025-06-01 17:09:23 -0500

    Archivos Borrados

--- footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-    </main>
-    <footer class="bg-light text-center py-3 mt-4">
-        <p>&copy; 2025 Marketing Valentina. Todos los derechos reservados.</p>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--- utils.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Debug logging function
-function debug_log($message) {
-    file_put_contents('debug.log', date('Y-m-d H:i:s') . " - $message\n", FILE_APPEND);
-}
-?>
